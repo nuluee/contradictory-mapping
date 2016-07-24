df6baa79be6b5765ff491896ba61ab17c42b20aa
nuluee <nuluee@gmail.com>
2016-07-24 15:58:47 +0800

    进入演出初版

--- js/all.js
+++ js/all.js
@@ -1,16 +1,50 @@
 $(function(){
 
+//loading动画
 
 
-	$('div#loadingimg img').animate({  
-		textIndent: 0 
-	},{
-   		step: function(now,fx) {
-      		$(this).css('-webkit-transform','rotateY('+now+'deg)'); 
-		    $(this).css('-moz-transform','rotateY('+now+'deg)');
-		    $(this).css('transform','rotateY('+now+'deg)'); 
-   		},
-    	duration:'slow'
-	},'linear');
+
+	$('div#loadingimg img')
+		.animate({  
+				textIndent: 0 
+			},{
+		   		step: function(now,fx) {
+		      		$(this).css('-webkit-transform','rotateY('+now+'deg)'); 
+				    $(this).css('-moz-transform','rotateY('+now+'deg)');
+				    $(this).css('transform','rotateY('+now+'deg)'); 
+		   		},
+		    	duration:'slow'
+			},'linear')//logo
+		.promise().done(function(){
+			$(this).parent().next()
+				.animate({width:'100%'})
+				.animate({height:'100%'})
+				.promise().done(function(){
+					$(this).next().fadeIn(1000);
+
+				}).promise().done(function(){
+					$(this).parents('#loading').fadeOut();
+					$(this).parents('#loading').next().fadeIn();
+
+				})
+	
+		})	
+
+			
+
+	
+
+
+
+
+
+
+
+
+
+
+		
+		
+
 
 });
