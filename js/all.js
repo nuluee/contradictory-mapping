92dfb9199a8e5cb8aaf6fcbc928b54b07999dafb
nuluee <nuluee@gmail.com>
2016-09-17 12:51:41 +0800

    loading页面缺动画版

--- js/all.js
+++ js/all.js
@@ -1,9 +1,6 @@
 $(function(){
 
 //loading动画
-
-
-
 	$('div#loadingimg img')
 		.animate({  
 				textIndent: 0 
@@ -19,16 +16,45 @@ $(function(){
 			$(this).parent().next()
 				.animate({width:'100%'})
 				.animate({height:'100%'})
-				.promise().done(function(){
-					$(this).next().fadeIn(1000);
+				.delay(1000)
+				.animate({top:'-196'})
+				.animate({width:'1%'})
+				.delay(2000)
+				.animate({
+					textIndent: -90,
+					width:'90%',
+				},{
 
-				}).promise().done(function(){
-					$(this).parents('#loading').fadeOut();
-					$(this).parents('#loading').next().fadeIn();
+					step: function(now,fx){
+
+						$(this).css('-webkit-transform','rotateZ('+now+'deg)'); 
+						$(this).css('-moz-transform','rotateZ('+now+'deg)');
+						$(this).css('transform','rotateZ('+now+'deg)'); 
+
+						},duration:500
+					},'linear')
+				.promise().done(function(){
+					$(this).animate({
+						width:'0',
+						top:'-200px'
+					})
+					$('div#loadingimg').animate({
+						width:'0',
+						height:'0'
+					})
+					$('div#loadingimg img').animate({
+						width:'0',
+						height:'0'
+					})
 
 				})
+				.promise().done(function(){
+					$('#loading').delay(1000).animate({
+						top:'100%'
+					})
+				})
 	
-		})	
+		})//promise	
 
 			
 
